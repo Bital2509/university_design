6cf53671d1a6977c09a90b1847b34d2a545e3a50
MoralFuck <bital2509@gmail.com>
2021-08-12 13:29:47 +0300

    creation and layout of a new page "Institute" Added new page "Institute" and layout

--- public/js/main.js
+++ public/js/main.js
@@ -10,3 +10,27 @@ $('#news-button-two').click(function () {
     $('#swiper-four').addClass('swiper-active');
     $('#swiper-three').removeClass('swiper-active');
 })
+$('#institute-button-one').click(function () {
+    $('#institute-button-one').addClass('menu__button-active')
+    $('#institute-button-two').removeClass('menu__button-active')
+    $('#institute-button-three').removeClass('menu__button-active')
+    $('#general-information').addClass('institute-block__active');
+    $('#staff').removeClass('institute-block__active');
+    $('#network-resources').removeClass('institute-block__active');
+})
+$('#institute-button-two').click(function () {
+    $('#institute-button-two').addClass('menu__button-active')
+    $('#institute-button-one').removeClass('menu__button-active')
+    $('#institute-button-three').removeClass('menu__button-active')
+    $('#staff').addClass('institute-block__active');
+    $('#general-information').removeClass('institute-block__active');
+    $('#network-resources').removeClass('institute-block__active');
+})
+$('#institute-button-three').click(function () {
+    $('#institute-button-three').addClass('menu__button-active')
+    $('#institute-button-one').removeClass('menu__button-active')
+    $('#institute-button-two').removeClass('menu__button-active')
+    $('#network-resources').addClass('institute-block__active');
+    $('#staff').removeClass('institute-block__active');
+    $('#general-information').removeClass('institute-block__active');
+})
